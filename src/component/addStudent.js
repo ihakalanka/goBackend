76a5202c7f2218f492ec0633e041d15e4f5274e5
akalankaih19@gmail.com <akalankaih19@gmail.com>
2021-11-03 22:24:17 +0530

    Form is implemented

--- src/component/addStudent.js
+++ src/component/addStudent.js
@@ -1,27 +1,29 @@
 import React from "react";
-import { Form,Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 
 export default function AddStudent(){
     return(
+        <Container>
+            <br/><br/><br/>
+            <h3>Student Registration Form</h3>
+            <br/>
         <Form>
-  <Form.Group className="mb-3" controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
+  <Form.Group className="mb-3">
+    <Form.Control type="number" placeholder="Index Number" />
   </Form.Group>
-
-  <Form.Group className="mb-3" controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
+  <Form.Group className="mb-3">
+    <Form.Control type="text" placeholder="Student Name" />
   </Form.Group>
-  <Form.Group className="mb-3" controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
+  <Form.Group className="mb-3">
+    <Form.Control type="number" placeholder="Age" />
+  </Form.Group>
+  <Form.Group className="mb-3">
+    <Form.Control type="email" placeholder="Email" />
   </Form.Group>
   <Button variant="primary" type="submit">
     Submit
   </Button>
 </Form>
+</Container>
     );
 }
