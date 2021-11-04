ac3fda5c32113f539c5879408b5ca51cade0f990
akalankaih19@gmail.com <akalankaih19@gmail.com>
2021-11-04 07:13:47 +0530

    Add a table and post method

--- src/component/addStudent.js
+++ src/component/addStudent.js
@@ -1,29 +1,93 @@
-import React from "react";
-import { Form, Button, Container } from "react-bootstrap";
+import axios from "axios";
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
 
-export default function AddStudent(){
-    return(
-        <Container>
-            <br/><br/><br/>
-            <h3>Student Registration Form</h3>
-            <br/>
-        <Form>
-  <Form.Group className="mb-3">
-    <Form.Control type="number" placeholder="Index Number" />
-  </Form.Group>
-  <Form.Group className="mb-3">
-    <Form.Control type="text" placeholder="Student Name" />
-  </Form.Group>
-  <Form.Group className="mb-3">
-    <Form.Control type="number" placeholder="Age" />
-  </Form.Group>
-  <Form.Group className="mb-3">
-    <Form.Control type="email" placeholder="Email" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
-</Container>
-    );
+export default function AddStudent() {
+  const [indexNumber, setIndexNumber] = useState("");
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [email, setEmail] = useState("");
+
+  const addStudent = (e) => {
+    e.preventDefault();
+
+    //create an student object
+    const newStudent = {
+      indexNumber,
+      name,
+      age,
+      email,
+    };
+
+    //create the post method to take frontend data to backend
+    axios
+      .post("API", newStudent)
+      .then((res) => {
+        alert("Student Added..");
+        e.target.reset();
+      })
+      .catch((err) => {
+        alert("Something went wrong");
+        console.log("Error is " + err);
+      });
+  };
+
+  return (
+    <div className="container">
+      <br />
+      <br />
+      <br />
+      <h3>Student Registration Form</h3>
+      <br />
+      <Form onSubmit={addStudent}>
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="number"
+            placeholder="Index Number"
+            onChange={(e) => {
+              setIndexNumber(e.target.value);
+            }}
+            required="true"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="text"
+            placeholder="Student Name"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            required="true"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="number"
+            placeholder="Age"
+            onChange={(e) => {
+              setAge(e.target.value);
+            }}
+            required="true"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Control
+            type="email"
+            placeholder="Email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            required="true"
+          />
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
--- src/component/allStudent.js
+++ src/component/allStudent.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Table } from "react-bootstrap";
 
-export default function AllStudent(){
-    return(
-        <h3>Hello World</h3>
-    );
+export default function AllStudent() {
+  return (
+    <div className="container">
+      <br />
+      <h3>All Students</h3>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>index Number</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>E-mail</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
-
