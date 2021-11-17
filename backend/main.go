b9e17c42f9957c94dd29c8354c7d73c902f6ec83
thilin12 <74877813+thilin12@users.noreply.github.com>
2021-11-17 22:45:26 +0530

    update new files1

--- backend/main.go
+++ backend/main.go
@@ -15,14 +15,11 @@ type Student struct{
 }
 
 var student []Student
-func getStudent(w http.ResponseWriter,r *http.Request){
-	w.Header().Set("Content-Type","Authorization")
-	json.NewEncoder(w).Encode(student)
-}
+
 func main() {
 student=append(student, Student{ID: "194005K",Name: "Akalanaka",Age: 23,E_mail: "Akalanaka@gmail.com"})
 	r :=mux.NewRouter()
-	r.HandleFunc("/api/getData",getStudent).Methods("GET")
+	
 	 http.ListenAndServe(":8080",r)
    
 }
